2a22016045b41c01fc989c0971c36a90d0a02a97
GeralBot <133059729+GeralBot@users.noreply.github.com>
2023-06-12 21:03:14 -0500

    Geral Bot

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = ` @𝐠𝐞𝐫𝐚𝐥_𝐯𝐞𝐧𝐭𝐚𝐬𝟏𝟖 : ${pesan}`
-let teks = `𝐑𝐞𝐯𝐢𝐯𝐚𝐧 𝐁𝐨𝐭𝐬 - 𝐆𝐞𝐫𝐚𝐥 𝐁𝐨𝐭 \n\n❏ ${oi}\n\n❏  𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 :\n`
+let oi = ` 𝐌𝐞𝐧𝐬𝐚𝐣𝐞 : ${pesan}`
+let teks = `𝐑𝐞𝐯𝐢𝐯𝐚𝐧 𝐏𝐫𝐨𝐬𝐭𝐢𝐬 \n\n❏ ${oi}\n\n❏  𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 :\n`
 for (let mem of participants) {
-teks +=`🩵💎🩵 @${mem.id.split('@')[0]}\n`}
-teks += `➥𝐆𝐞𝐫𝐚𝐥 𝐁𝐨𝐭 `
+teks +=`💎 @${mem.id.split('@')[0]}\n`}
+teks += `➥𝐌𝐢𝐥𝐮𝐯𝐢𝐩 `
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
